dc14ad6ff1fb9b3f18b21b129bb2d42b8ccb9679
HailDevil <68913506+HailDevil@users.noreply.github.com>
2020-08-01 11:49:06 +0530

    Create waifu.py

---
+++ plugins/waifu.py
@@ -0,0 +1,97 @@
+""" Creates random anime sticker """
+
+import random
+import re
+from userge import userge, Message
+
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+")
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
+
+
+@userge.on_cmd("waifu", about={
+    'header': "Creates random anime sticker",
+    'flags': {
+        '-r': "To get random sticker",
+        '-g': "To get google search sticker",
+        '-mock': "get mock text in sticker"},
+    'usage': "{tr}waifu [text | reply to message]\n"
+             "{tr}waifu [flags] [text | reply to message]",
+    'examples': [
+        "{tr}waifu Oni-Chan",
+        "{tr}waifu [flags] Oni-Chan"]}, allow_via_bot=False)
+async def anime_sticker(message: Message):
+    """ Creates random anime sticker! """
+    replied = message.reply_to_message
+    args = message.filtered_input_str
+    if args:
+        text = args
+    elif replied:
+        text = args if args else replied.text
+    else:
+        await message.err("```Input not found!...```", del_in=3)
+        return
+    await message.delete()
+    if '-g' in message.flags:
+        try:
+            stickers = await userge.get_inline_bot_results(
+                "stickerizerbot",
+                f"#12{deEmojify(text)}")
+            await userge.send_inline_bot_result(
+                chat_id=message.chat.id,
+                query_id=stickers.query_id,
+                result_id=stickers.results[0].id,
+                hide_via=True)
+        except IndexError:
+            await message.edit("```List index out of range```", del_in=3)
+        else:
+            await message.delete()
+            return
+    if '-r' in message.flags:
+        k = [1, 3, 7, 9, 13, 22, 34, 35, 36, 37, 43, 44, 45, 52, 53, 55]
+        animus = random.choice(k)
+    elif '-mock' in message.flags:
+        animus = 7
+    else:
+        k = [20, 32, 33, 40, 41, 42, 58]
+        animus = random.choice(k)
+       
+    try:
+        stickers = await userge.get_inline_bot_results(
+            "stickerizerbot",
+            f"#{animus}{deEmojify(text)}"
+        )
+        saved = await userge.send_inline_bot_result(
+            chat_id="me",
+            query_id=stickers.query_id,
+            result_id=stickers.results[0].id,
+            hide_via=True
+        )
+        saved = await userge.get_messages("me", int(saved.updates[1].message.id))
+        message_id = replied.message_id if replied else None
+        await userge.send_sticker(
+            chat_id=message.chat.id,
+            sticker=str(saved.sticker.file_id),
+            file_ref=str(saved.sticker.file_ref),
+            reply_to_message_id=message_id
+        )
+        await saved.delete()
+    except IndexError:
+        await message.edit("```List index out of range```", del_in=3)
